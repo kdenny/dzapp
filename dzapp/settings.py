ec655e6a5c95f33a003652b04bc15e9d5689afac
Kevin Denny <kdenny37@gmail.com>
2019-10-10 23:29:20 -0400

    changed settings

--- dzapp/settings.py
+++ dzapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'samples',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'dzapp.wsgi.application'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Database
